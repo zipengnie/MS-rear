88c45d10bf8fe9dfadc7a0d6f6149fe2b37b124c
Eagle <zipengnie@outlook.com>
2018-11-30 22:44:18 +0800

    实现用户管理页面分页功能

--- server.js
+++ server.js
@@ -48,20 +48,17 @@ app.post("/api/userCheck", function (req, res) {
     })
 })
 
-
 //注册的请求
 app.post("/api/register.html", function (req, res) {
     //设置响应头来处理跨域问题
     res.set({ "Access-Control-Allow-Origin": "*" });
-
     //1.获取前端传递过来的参数
     var name = req.body.name;
     var pwd = req.body.pwd;
     var nickname = req.body.nickname;
     var age = req.body.age;
-    var sex = req.body.sex;
-    var isAdmin = req.body.isAdmin == "是" ? true : false;
-
+    var sex = req.body.sex == "true" ? "男" : "女";
+    var isAdmin = req.body.isAdmin == "true" ? "是" : "否";
     var result = {}; //
     // 注册的时候需要先校验用户名是否已注册过
     MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
@@ -181,7 +178,8 @@ app.post("/api/login.html", function (req, res) {
                 result.code = 0;//非0表示错误
                 result.message = "登录成功";
                 result.data = {
-                    nickname: docs[0].nickname
+                    nickname: docs[0].nickname,
+                    isAdmin: docs[0].isAdmin
                 }
             }
             //关闭数据库连接
@@ -191,80 +189,187 @@ app.post("/api/login.html", function (req, res) {
     })
 })
 
-    //用户页面信息请求
-    app.post("/api/userInfo.html", function (req, res) {
-        //设置响应头来处理跨域问题
-        res.set({ "Access-Control-Allow-Origin": "*" });
+//用户管理页面渲染请求
+app.get("/api/userInfo.html", function (req, res) {
+    //设置响应头来处理跨域问题
+    res.set({ "Access-Control-Allow-Origin": "*" });
 
-        //1.获取前端传递过来的参数
-        var page = parseInt(req.query.page) || 1;//初始化默认页码1
-        var pageSize = parseInt(req.query.pageSize) || 5;//初始化默认每页显示5条数据
-        var totalSize = 0;//数据总条数
-        var totalPage = 0;//总页数
+    //1.获取前端传递过来的参数
+    var page = parseInt(req.query.page) || 1;//初始化默认页码1
+    var pageSize = parseInt(req.query.pageSize) || 2;//初始化默认每页显示5条数据
+    var totalSize = 0;//数据总条数
+    var totalPage = 0;//总页数
+    var result = {};
 
-        var result = {};
-        // 连接服务器和数据库
-        MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+    // 连接服务器和数据库
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {
+            result.code = -1;//非0表示错误
+            result.message = "连接服务器失败";
+            res.json(result);
+            return;
+        }
+        //连接数据库
+        var db = client.db("MS");
+
+        async.series([
+            function (cb) {
+                //查询MongoDB数据
+                db.collection("user").find().count(function (error, num) {
+                    if (error) {
+                        result.code = -1;//非0表示错误
+                        result.message = "查询失败";
+                        cb(result.code, result);
+                    } else {
+                        result.code = 0;//非0表示错误
+                        result.message = "查询成功";
+                        result.totalSize = num;//取数据库查询总数据条数
+                        cb(null, result);
+                    }
+                })
+            },
+            //获取当前页码的几条数据
+            function (cb) {
+                //查询MongoDB数据
+                db.collection("user").find().limit(pageSize).skip(page * pageSize - pageSize).toArray(function (error, docs) {
+                    if (error) {
+                        result.code = -1;//非0表示错误
+                        result.message = "查询数据失败";
+                        cb(result.code, result);
+                    } else {
+                        result.code = 0;//非0表示错误
+                        result.message = "查询数据成功";
+                        result.data = docs //当前页面数据
+                        cb(null, result);
+                    }
+                })
+            }], function (error, rel) {//注意：data是一个数组，前面没有传参[undefined,docs]
+                if (error == null) {
+                    // 当前页码
+                    result.page = page;
+                    // 每页显示数据条数
+                    result.pageSize = pageSize;
+                    // 总页数
+                    result.totalPage = Math.ceil(result.totalSize / result.pageSize);
+                    res.json(result);
+                }
+                //关闭连接
+                client.close();
+            })
+    })
+})
+
+// 用户管理页面搜索请求(默认查询昵称数据)
+app.get("/api/userInfo/search", function (req, res) {
+    //设置响应头来处理跨域问题
+    res.set({ "Access-Control-Allow-Origin": "*" });
+    // 要查询的关键字
+    var searchVal = req.query.searchVal;
+    var result = {};
+    // 正则模糊匹配
+    var filter = new RegExp(searchVal);
+    // 连接服务器
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {
+            result.code = -1;//非0表示错误
+            result.message = "连接服务器失败";
+            res.json(result);
+            return;
+        }
+        //连接数据库
+        var db = client.db("MS")
+        //查询数据
+        db.collection("user").find({ nickname: filter }).toArray(function (error, docs) {
             if (error) {
                 result.code = -1;//非0表示错误
-                result.message = "连接服务器失败";
+                result.message = "查询数据失败";
                 res.json(result);
-                return;
+            } else if (docs.length <= 0) {
+                result.code = 0;//非0表示错误
+                result.message = "查询0条数据";
+                res.json(result);
+            } else {
+                result.code = 0;//非0表示错误
+                result.message = "查询" + docs.length + "条数据";
+                result.data = docs;
+                res.json(result);
             }
-            //连接数据库
-            var db = client.db("MS");
-
-            async.series([
-                function (cb) {
-                    //查询MongoDB数据
-                    db.collection("user").find().count(function (error, num) {
-                        if (error) {
-                            result.code = -1;//非0表示错误
-                            result.message = "查询失败";
-                            cb(result.code, result);
-                        } else {
-                            result.code = 0;//非0表示错误
-                            result.message = "查询成功";
-                            result.data = {
-                                totalSize: num//取数据库查询总数据条数
-                            }
-                            cb(null, result);
-                        }
-                    })
-                },
-                //获取当前页码的几条数据
-                function (cb) {
-                    //查询MongoDB数据
-                    db.collection("user").find().limit(pageSize).skip(page * pageSize - pageSize).toArray(function (error, docs) {
-                        if (error) {
-                            result.code = -1;//非0表示错误
-                            result.message = "查询数据失败";
-                            cb(result.code, result);
-                        } else {
-                            result.code = 0;//非0表示错误
-                            result.message = "查询数据成功";
-                            result.data = {
-                                docs:docs //当前页面数据
-                            }
-                            cb(null,result);
-                        }
-                    })
-                }], function (error, data) {//注意：data是一个数组，前面没有传参[undefined,docs]
-                    if (error) {
-                        res.json(result);
-                    } else {
-                        //获取总页数
-                        totalPage = Math.ceil(result.data.totalSize / pageSize);
-                        result.data = {
-                            totalPage:totalPage,
-                            pageSize: pageSize, 
-                            page: page
-                        }
-                    }
-                    //关闭连接
-                    client.close();
-                })
         })
+        //关闭数据库连接
+        client.close();
     })
+})
 
+// 删除用户信息
+app.get("/api/userInfo/delete", function (req, res) {
+    //设置响应头来处理跨域问题
+    res.set({ "Access-Control-Allow-Origin": "*" });
+    // 要查询的关键字
+    var name = req.query.name;
+    var result = "";
+    // 连接服务器
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {
+            result.code = -1;//非0表示错误
+            result.message = "连接服务器失败";
+            res.json(result);
+            return;
+        }
+        //连接数据库
+        var db = client.db("MS")
+
+        //串行无关联async.series
+        async.series([
+            //删除用户
+            function (cb) {
+                //插入数据到MongoDB中
+                db.collection("user").remove({ userName: name }, function (error) {
+                    if (error) {
+                        result.code = -1;//非0表示错误
+                        result.message = "删除失败";
+                        cb(result.code, result);
+                        return;
+                    } else {
+                        result.code = 0;//非0表示错误
+                        result.message = "删除成功";
+                        cb(null, result);
+                    }
+                })
+            },
+            //确认是否真正删除成功
+            function (cb) {
+                db.collection("user").find({ userName: name }).toArray(function (error, data) {
+                    if (error) {
+                        result.code = -1;//非0表示错误
+                        result.message = "查询失败";
+                        cb(result.code, result);
+                    } else if (data) {
+                        result.code = -1;//非0表示错误
+                        result.message = "确认删除失败";
+                        cb(result.code, result);
+                    } else {
+                        result.code = 0;//非0表示错误
+                        result.message = "确认删除成功";
+                        cb(null, result);
+                    }
+                })
+            }], function (error, result) {
+                if (error) {
+                    res.json(error);
+                } else {
+                    res.json(result);
+                }
+                //关闭数据库连接
+                client.close();
+            })
+    })
+})
+
+//新增手机数据请求(手机)
+app.get("/api/phone/add", function (req, res) {
+    //设置响应头来处理跨域问题
+    res.set({ "Access-Control-Allow-Origin": "*" });
+
+
+})
 app.listen(3000)
